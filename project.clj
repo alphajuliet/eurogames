2ddc0612123fe02584e25351c6b5d4e4f8e00f2a
alphajuliet <andrew@alphajuliet.com>
2020-06-27 16:18:21 +1000

    Get game details from XML via zippers.

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [clj-http "3.10.1"]
                  [cheshire "5.10.0"]
-                 [org.clojure/data.xml "0.0.8"]]
+                 [org.clojure/data.xml "0.0.8"]
+                 [org.clojure/data.zip "1.0.0"]]
   :repl-options {:init-ns eurogames.core})
--- src/eurogames/core.clj
+++ src/eurogames/core.clj
@@ -1,7 +1,9 @@
 (ns eurogames.core
   (:require [clj-http.client :as client]
             [cheshire.core :as json]
-            [clojure.data.xml :as x]))
+            [clojure.data.xml :as xml]
+            [clojure.zip :as zip]
+            [clojure.data.zip.xml :as zx]))
 
 (defn get-airtable-records
   "Read a single page of records from Airtable."
@@ -38,9 +40,39 @@
   "Get details of a given game"
   [game-id]
   (let [uri (str "https://www.boardgamegeek.com/xmlapi/boardgame/" game-id)]
-    (-> (client/get uri {:query {:stats 1}})
+    (-> (client/get uri {:query-params {:stats 1}})
         (:body)
         (java.io.StringReader.)
-        (x/parse))))
+        (xml/parse)
+        (zip/xml-zip))))
+
+(defn game-details
+  [zipper]
+  {:name (zx/xml1-> zipper 
+                    :boardgames 
+                    :boardgame 
+                    :name 
+                    zx/text)
+   :year (zx/xml1-> zipper 
+                    :boardgames 
+                    :boardgame 
+                    :yearpublished 
+                    zx/text)
+   :weight (zx/xml1-> zipper 
+                      :boardgames 
+                      :boardgame 
+                      :statistics 
+                      :ratings 
+                      :averageweight 
+                      zx/text)
+   :ranking (zx/xml1-> zipper 
+                       :boardgames 
+                       :boardgame 
+                       :statistics
+                       :ratings 
+                       :ranks 
+                       :rank
+                       (zx/attr= :name "boardgame")
+                       (zx/attr :value))})
 
 ;; The End
