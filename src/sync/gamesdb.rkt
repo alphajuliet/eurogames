5ca3074e1357cdd6cf1264416918737e60e6d8df
alphajuliet <andrew@alphajuliet.com>
2024-02-25 10:34:19 +1100

    Refactor and update ID query

--- src/sync/gamesdb.rkt
+++ src/sync/gamesdb.rkt
@@ -7,11 +7,15 @@
 
 (provide (all-defined-out))
 
-(define gdb (sqlite3-connect
-             #:database "/Users/andrew/Documents/Projects/games/eurogames/data/games.db"))
+(define SQLDB "/Users/andrew/Documents/Projects/games/eurogames/data/games.db")
+(define gdb (sqlite3-connect #:database SQLDB))
 
 (define (db-get-all-ids)
-  (query-list gdb "SELECT id FROM games ORDER BY id"))
+  (query-list gdb "SELECT id FROM notes ORDER BY id"))
+
+;; Update the table with data from a given CSV file. 
+(define (db-update-game-data csvfile)
+  #f)
 
 (module+ main
   (db-get-all-ids))
