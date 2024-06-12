6f6bcb8dfc8aba6507cab70b331baf5bca322b69
alphajuliet <andrew@alphajuliet.com>
2024-06-12 15:54:41 +1000

    Add new game to Notes table

--- data/add-game.sh
+++ data/add-game.sh
@@ -8,6 +8,11 @@ else
   CSV=$1
 fi
 
+echo "Adding game to table: bgg"
 sqlite-utils insert games.db bgg ${CSV} --csv
+
+echo "Adding game to table: notes"
+GAME_ID=`basename $1 .csv`
+sql games.db "insert into notes (id, status, platform) values ($GAME_ID, 'Evaluating', 'BGA');"
 
 # The End
