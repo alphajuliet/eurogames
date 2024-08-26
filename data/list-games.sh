26d83459820efd7abb5215260782f9d39574967d
alphajuliet <andrew@alphajuliet.com>
2024-08-26 11:56:41 +1000

    Add list-games script

---
+++ data/list-games.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Return the names of all the games we are playing
+
+if [ -z "$1" ]; then
+  STATUS="Playing"
+else
+  STATUS="$1"
+fi
+  
+DB="games.db"
+QUERY="SELECT bgg.name FROM notes
+  LEFT JOIN bgg ON notes.id = bgg.id
+  WHERE status = '${STATUS}'
+  ORDER BY name ASC"
+
+sqlite-utils ${DB} "${QUERY}" --table
+
+# The End
