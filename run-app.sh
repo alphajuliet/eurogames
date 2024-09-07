04b7e7ae7e0e01d6a776a8b31dce45eb0ec3f20c
alphajuliet <andrew@alphajuliet.com>
2024-09-07 16:35:12 +1000

    Fail gracefully if no venv session.

--- run-app.sh
+++ run-app.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-(cd src/app; flask run)
-
+if [ -z "$VIRTUAL_ENV" ]; then
+  echo "No virtual environment is active."
+else
+  echo "Virtual environment is active: $VIRTUAL_ENV"
+  (cd src/app; flask run)
+fi
