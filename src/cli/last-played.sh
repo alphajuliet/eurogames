c03f8b0b70cfe1e664da1e85904195fb536adce2
alphajuliet <andrew@alphajuliet.com>
2025-03-17 08:55:09 +1100

    Add optional limit to last-played

--- src/cli/last-played.sh
+++ src/cli/last-played.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # Return a table of the last n played games
 
+if [ -z "$1" ]; then
+  MAX=1000
+else
+  MAX="$1"
+fi
+
 DB="../../data/games.db"
-sqlite-utils $DB "SELECT * FROM last_played" --table
+sqlite-utils $DB "SELECT * FROM last_played LIMIT $MAX" --table
 
 # The End
