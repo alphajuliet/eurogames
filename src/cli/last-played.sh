efd410669476f54bb22d312acf27b9c55f15f54d
alphajuliet <andrew@alphajuliet.com>
2025-03-16 19:57:07 +1100

    Add 'last' command to CLI

--- src/cli/last-played.sh
+++ src/cli/last-played.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 # Return a table of the last n played games
 
-if [ -z "$1" ]; then
-  limit=10
-else
-  limit=$1
-fi
-
 DB="../../data/games.db"
-sqlite-utils $DB "SELECT * FROM played LIMIT $limit" -t
+sqlite-utils $DB "SELECT * FROM last_played" --table
 
 # The End
